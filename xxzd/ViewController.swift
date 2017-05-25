e8822bbb37ec21f6d8f5b81932f6d4ae3ed1ca29
Zhang LiangZhi <3717591@qq.com>
2017-05-26 02:00:59 +0800

    o

---
+++ xxzd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xxzd
+//
+//  Created by ZhangLiangZhi on 2017/5/26.
+//  Copyright © 2017年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
